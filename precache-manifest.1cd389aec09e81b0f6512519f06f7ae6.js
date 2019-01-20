4380e2f82473d27432ed7296c09d6b3d1094de28
Jason Lee <lcycug@live.com>
2019-01-20 13:49:34 +0800

    Updates

---
+++ precache-manifest.1cd389aec09e81b0f6512519f06f7ae6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b497ff24f3ed48766614",
+    "url": "/react-router-app/static/js/runtime~main.b497ff24.js"
+  },
+  {
+    "revision": "d0e4d6bd341909e20448",
+    "url": "/react-router-app/static/js/main.d0e4d6bd.chunk.js"
+  },
+  {
+    "revision": "20b5c858c74e8cb0726a",
+    "url": "/react-router-app/static/js/1.20b5c858.chunk.js"
+  },
+  {
+    "revision": "d0e4d6bd341909e20448",
+    "url": "/react-router-app/static/css/main.ab06c89f.chunk.css"
+  },
+  {
+    "revision": "3bc87da3bd62419f81a140a279ad446b",
+    "url": "/react-router-app/index.html"
+  }
+];
